5f4356e5548e4206461e0d3a31d9d86e98925863
isaQuant <isabella.quantschnig@gmx.net>
2021-10-27 11:57:05 +0200

    Added getter and setter in class 'task' and method printTask

--- scr/Task.java
+++ scr/Task.java
@@ -1,8 +1,50 @@
+import java.util.Date;
+
 public class Task {
 
 
     /**
      * If creating a task, it will be automatically added to the list
      */
+    private String title;
+    private String description;
+    private Date dueDate;
 
+    /**
+     * Constructor, only the title is needed for creating a task
+     * @param title
+     */
+    public Task(String title) {
+        this.title = title;
+    }
+
+    /*** Getter and Setter ***/
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    /**
+     * Prints title, due date and description of the task
+     */
+    public void printTask() {
+        System.out.println("Title: " + this.getTitle());
+        System.out.println("DueDate: " + this.getDueDate());
+        System.out.println("Description: " + this.getDescription());
+    }
 }

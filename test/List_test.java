b5356c0378cfef8ac0ba4a51e9aa612db89015c7
isaQuant <isabella.quantschnig@gmx.net>
2021-10-27 19:42:37 +0200

    Added test class

--- test/List_test.java
+++ test/List_test.java
@@ -1,9 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class List_test {
-/*
+
+    // testobjects
+    Task t1 = new Task("Task Nr.1");
+    Task t2 = new Task("Task Nr.2");
+    Task t3 = new Task("Task Nr.3");
+
+
+
+    @BeforeEach
+    public void clear() {
+
+
+    }
+
     @Test
- */
+    public void testTasklist() {
+
+    }
+
 
 
 
